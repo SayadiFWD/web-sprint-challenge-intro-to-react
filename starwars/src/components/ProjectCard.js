f5754645c98a3dc5d7a8637cd7664d6dc27a96e9
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-06-30 11:40:10 -0700

    changed-the-styling

--- starwars/src/components/ProjectCard.js
+++ starwars/src/components/ProjectCard.js
@@ -5,7 +5,71 @@ import styled, { css } from "styled-components";
 const Data = styled.div`
 border: 2px solid black;
 margin: 50px;
+padding: 150px;
+
+}
+
 `
+const Title = styled.h1`
+color: tomato;
+`
+
+const ListOne = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListTwo = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListThree = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListFour = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListFive = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListSix = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListSeven = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListEight = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+const ListNine = styled.ul`
+
+&:hover{
+  color: orange;
+}
+`
+
+
 
 
 const ProjectCard = ({
@@ -28,16 +92,16 @@ const ProjectCard = ({
 
   return (
     <Data className = 'wrapper'>
-      <h1>{name}</h1>
-      <ul>Created : {created}</ul>
-      <ul>Clinate : {climate}</ul>
-      <ul>Diameter : {diameter}</ul>
-      <ul>Edited : {edited}</ul>
-      <ul>Gravity : {gravity}</ul>
-      <ul>Population : {population}</ul>
-      <ul>Rotation : {rotation}</ul>
-      <ul>Surface : {surface}</ul>
-      <ul>Terrain : {terrain}</ul>
+      <Title>{name}</Title>
+      <ListOne>Created : {created}</ListOne>
+      <ListTwo>Clinate : {climate}</ListTwo>
+      <ListThree>Diameter : {diameter}</ListThree>
+      <ListFour>Edited : {edited}</ListFour>
+      <ListFive>Gravity : {gravity}</ListFive>
+      <ListSix>Population : {population}</ListSix>
+      <ListSeven>Rotation : {rotation}</ListSeven>
+      <ListEight>Surface : {surface}</ListEight>
+      <ListNine>Terrain : {terrain}</ListNine>
     </Data>
   );
 };
